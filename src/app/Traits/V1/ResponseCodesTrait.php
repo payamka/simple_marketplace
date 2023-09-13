6d11d8ea958da824631960bc253b57171f4f0c7a
payamka <payam.karimyan@gmail.com>
2023-09-13 16:08:37 +0330

    [ADD] init project

---
+++ src/app/Traits/V1/ResponseCodesTrait.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Traits\V1;
+
+trait ResponseCodesTrait
+{
+    const RESPONSE_FAIL = -1;
+    const RESPONSE_OK = 100;
+    const RESPONSE_NOT_AUTHORIZED = 99;
+}
---
+++ src/app/Traits/V1/ResponseGeneratorTrait.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Traits\V1;
+
+trait ResponseGeneratorTrait
+{
+    protected bool $has_error;
+    protected int $header_code;
+    protected string $response_code;
+
+    private function setHasError($has_error): self
+    {
+        $this->has_error = $has_error;
+
+        return $this;
+    }
+
+    private function setHeaderCode($header_code): self
+    {
+        $this->header_code = $header_code;
+
+        return $this;
+    }
+
+    private function setResponseCode($response_code): self
+    {
+        $this->response_code = $response_code;
+
+        return $this;
+    }
+
+    public function getResponse($data): string
+    {
+        $responseJson = ['hasError' => $this->has_error, 'responseCode' => $this->response_code];
+        if ($this->has_error) $responseJson['errors'] = $data; else $responseJson['data'] = $data;
+
+        return response()->json($responseJson, $this->header_code);
+    }
+
+    public function success($data, $response_code, $header_code)
+    {
+        return $this->setHasError(false)->setHeaderCode($header_code)->setResponseCode($response_code)
+            ->getResponse($data);
+    }
+
+    public function error($data, $response_code, $header_code)
+    {
+        return $this->setHasError(true)->setHeaderCode($response_code)->setResponseCode($response_code)
+            ->getResponse($data);
+    }
+}
